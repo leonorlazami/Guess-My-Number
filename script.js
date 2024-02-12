e995654316111ffa85a54f00838d7e11657bb1a1
theLeoNoir <lazami.leonor@gmail.com>
2024-02-12 19:21:30 +0100

    added eventListener

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
 'use strict';
+
+document.querySelector('.check').addEventListener('click', function (e) {
+
+    const guess = Number(document.querySelector('.guess').value)
+    console.log(guess)
+
+    if (!guess) {
+        document.querySelector('.message').textContent = 'No Number!'
+    }
+
+})
