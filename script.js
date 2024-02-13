eeb25182a5a65f692b40357f8e9aefaff858d829
theLeoNoir <lazami.leonor@gmail.com>
2024-02-13 14:29:00 +0100

    refactor code

--- script.js
+++ script.js
@@ -1,74 +1,78 @@
 'use strict';
 
-
-
-let secretNumber = Math.trunc(Math.random() * 20)
-
-
+let secretNumber = Math.trunc(Math.random() * 20);
 let score = 20;
+let highscore = 0;
 
+const displayMessage = function (message) {
+    document.querySelector('.message').textContent = message
 
-
+}
 
 document.querySelector('.check').addEventListener('click', function (e) {
-    const guess = Number(document.querySelector('.guess').value)
+    const guess = Number(document.querySelector('.guess').value);
+
     if (!guess) {
-        document.querySelector('.message').textContent = 'No Number!'
+        displayMessage('No Number!')
     } else if (guess === secretNumber) {
-        document.querySelector('.message').textContent = 'Correct Number!'
-        document.querySelector('.number').textContent = secretNumber
+        displayMessage('Correct Number!')
+        document.querySelector('.number').textContent = secretNumber;
         document.querySelector('body').style.backgroundColor = '#60b347';
-        document.querySelector('.number').style.width = '30rem'
+        document.querySelector('.number').style.width = '30rem';
 
-
-    } else if (guess > secretNumber) {
-        if (score > 1) {
-            document.querySelector('.message').textContent = 'Too high!'
-            score--;
-            document.querySelector('.score').textContent = score
-        } else {
-            document.querySelector('.message').textContent = 'You lost the game'
-            document.querySelector('.score').textContent = 0
+        if (score > highscore) {
+            highscore = score
+            document.querySelector('.highscore').textContent = highscore
         }
-    } else if (guess < secretNumber) {
-        if (score > 1) {
-            document.querySelector('.message').textContent = 'Too low'
-            score--;
-            document.querySelector('.score').textContent = score
-        } else {
-            document.querySelector('.message').textContent = 'You lost the game'
-            document.querySelector('.score').textContent = 0
 
+
+        if (isNightMode) {
+            toggleMode();
+            updateNightModeStyles();
+        }
+
+
+
+    } else if (guess !== secretNumber) {
+        if (score > 1) {
+            displayMessage(guess > secretNumber ? 'Too high' : 'Too low')
+            score--;
+            document.querySelector('.score').textContent = score;
+        } else {
+            displayMessage('You lost the game')
+            document.querySelector('.score').textContent = 0;
         }
     }
 
-})
-
+});
 
 document.querySelector('.again').addEventListener('click', function () {
-    document.querySelector('.score').textContent = 20
-    document.querySelector('body').style.backgroundColor = '#222'
+    document.querySelector('.score').textContent = 20;
+    document.querySelector('body').style.backgroundColor = '#222';
     document.querySelector('.guess').value = '';
-    document.querySelector('.message').textContent = 'No Number!'
+    document.querySelector('.message').textContent = 'Start guessing!';
     document.querySelector('.number').textContent = '?';
-    document.querySelector('.number').style.width = '15rem'
-    secretNumber = Math.trunc(Math.random() * 20)
+    document.querySelector('.number').style.width = '15rem';
+    secretNumber = Math.trunc(Math.random() * 20);
+    document.querySelector('.highscore').textContent = highscore
+});
 
-})
+let isNightMode = false;
 
-
-let isNightMode = false
 function toggleMode() {
-    isNightMode = !isNightMode
+    isNightMode = !isNightMode;
 }
-document.querySelector('.nightmode').addEventListener('click', function () {
-    toggleMode()
-    document.querySelector('body').classList.toggle('toggle-night-mode')
-    document.querySelector('header').classList.toggle('toggle-night-mode')
-    document.querySelector('main').classList.toggle('toggle-night-mode')
-    document.querySelector('.guess').classList.toggle('toggle-night-mode')
+
+function updateNightModeStyles() {
+    document.querySelector('body').classList.toggle('toggle-night-mode');
+    document.querySelector('header').classList.toggle('toggle-night-mode');
+    document.querySelector('main').classList.toggle('toggle-night-mode');
+    document.querySelector('.guess').classList.toggle('toggle-night-mode');
     document.querySelector('.nightmode').style.backgroundColor = isNightMode ? '#222' : 'white';
-    document.querySelector('.nightmode').textContent = isNightMode ? '🌞' : '🌙'
+    document.querySelector('.nightmode').textContent = isNightMode ? '🌞' : '🌙';
+}
 
-
-})
+document.querySelector('.nightmode').addEventListener('click', function () {
+    toggleMode();
+    updateNightModeStyles();
+});
