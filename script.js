65d0030764539a03102e2a46cf055adb63a80f43
theLeoNoir <lazami.leonor@gmail.com>
2024-02-13 13:06:34 +0100

    add nightmode

--- script.js
+++ script.js
@@ -2,11 +2,11 @@
 
 
 
-const secretNumber = Math.trunc(Math.random() * 20)
-console.log('number', secretNumber)
+let secretNumber = Math.trunc(Math.random() * 20)
+
 
 let score = 20;
-let highscore = 0
+
 
 
 
@@ -17,8 +17,10 @@ document.querySelector('.check').addEventListener('click', function (e) {
     } else if (guess === secretNumber) {
         document.querySelector('.message').textContent = 'Correct Number!'
         document.querySelector('.number').textContent = secretNumber
-        highscore++;
-        document.querySelector('.highscore').textContent = highscore
+        document.querySelector('body').style.backgroundColor = '#60b347';
+        document.querySelector('.number').style.width = '30rem'
+
+
     } else if (guess > secretNumber) {
         if (score > 1) {
             document.querySelector('.message').textContent = 'Too high!'
@@ -29,9 +31,7 @@ document.querySelector('.check').addEventListener('click', function (e) {
             document.querySelector('.score').textContent = 0
         }
     } else if (guess < secretNumber) {
-
         if (score > 1) {
-
             document.querySelector('.message').textContent = 'Too low'
             score--;
             document.querySelector('.score').textContent = score
@@ -41,5 +41,34 @@ document.querySelector('.check').addEventListener('click', function (e) {
 
         }
     }
+
+})
+
+
+document.querySelector('.again').addEventListener('click', function () {
+    document.querySelector('.score').textContent = 20
+    document.querySelector('body').style.backgroundColor = '#222'
+    document.querySelector('.guess').value = '';
+    document.querySelector('.message').textContent = 'No Number!'
+    document.querySelector('.number').textContent = '?';
+    document.querySelector('.number').style.width = '15rem'
+    secretNumber = Math.trunc(Math.random() * 20)
+
+})
+
+
+let isNightMode = false
+function toggleMode() {
+    isNightMode = !isNightMode
+}
+document.querySelector('.nightmode').addEventListener('click', function () {
+    toggleMode()
+    document.querySelector('body').classList.toggle('toggle-night-mode')
+    document.querySelector('header').classList.toggle('toggle-night-mode')
+    document.querySelector('main').classList.toggle('toggle-night-mode')
+    document.querySelector('.guess').classList.toggle('toggle-night-mode')
+    document.querySelector('.nightmode').style.backgroundColor = isNightMode ? '#222' : 'white';
+    document.querySelector('.nightmode').textContent = isNightMode ? '🌞' : '🌙'
+
 
 })
